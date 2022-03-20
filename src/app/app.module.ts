d522aec627efadb75df7f528e51b01c569865822
hormalno <ivanov.yasen@gmail.com>
2022-03-20 23:58:15 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from 'src/app/core/core.module';
+import { FooterComponent } from 'src/app/core/footer/footer.component';
+import { HeaderComponent } from 'src/app/core/header/header.component';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { PagesModule } from './feature/pages/pages.module';
+import { ThemesModule } from './feature/themes/themes.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
+    CoreModule,
+    AuthModule,
+    PagesModule,
+    ThemesModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/feature/pages/pages.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class PagesModule { }
